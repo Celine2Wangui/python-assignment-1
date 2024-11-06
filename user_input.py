5aee5536b2ea79b07fd42a0274efa4a54386e440
Celine2Wangui <celinewangui00@gmail.com>
2024-11-06 21:50:12 +0300

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+location = input("Enter your location: ")
+
+# Personalized message
+message = f"Hello {name}, you are {age} years old and live in {location}."
+
+print(message)
